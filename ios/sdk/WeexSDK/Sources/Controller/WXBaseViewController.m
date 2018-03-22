2a9cd6c1c71f0c14c6f8513b8cfb54da1d563673
hyj223 <huyujin19905@gmail.com>
2018-03-22 17:49:33 +0800

    ios:::weex nav height 可定制

--- ios/sdk/WeexSDK/Sources/Controller/WXBaseViewController.m
+++ ios/sdk/WeexSDK/Sources/Controller/WXBaseViewController.m
@@ -249,6 +249,7 @@
 //        _titleColor = [UIColor colorWithRed:56/255.f green:81/255.f blue:152/255.f alpha:1.f];
     NSString *ucx_navBarColor = @"#385198";
     NSString *ucx_backgroundColor = @"#f4f4f1";
+    CGFloat ucx_navBarHeight = 128.f;
     if ([self.ucx_param count]>0) {
         NSDictionary *ucx_navBar = [self.ucx_param objectForKey:@"navBar"];
         if ([ucx_navBar count]>0 && [ucx_navBar isKindOfClass:[NSDictionary class]]) {
@@ -260,12 +261,18 @@
             if (backgroundColor) {
                 ucx_backgroundColor = backgroundColor;
             }
+            
+            NSNumber *navBarHeight = [ucx_navBar objectForKey:@"height"];
+            if (navBarHeight) {
+                ucx_navBarHeight = [navBarHeight floatValue];
+            }
+            
         }
     }
     
     [self.naviView removeFromSuperview];
     float width = [UIScreen mainScreen].bounds.size.width;
-    self.naviView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, width, 128*width/750)];
+    self.naviView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, width, ucx_navBarHeight*width/750)];
     [self.naviView setBackgroundColor:[WXConvert UIColor:ucx_navBarColor]];
     
     [self.view addSubview:self.naviView];
